90c6c8d27347cefff8239cbbae6ff6ec6f02f208
nohz.afk <149959021+nohzafk@users.noreply.github.com>
2025-11-22 23:18:04 +0100

    add load env file function

--- lisp/helper.el
+++ lisp/helper.el
@@ -13,3 +13,29 @@
 (defun emacs-backbone--format-plists-for-json (plists)
   "Convert a list of plist to a JSON array of objects"
   (mapcar #'emacs-backbone--plist-to-alist plists))
+
+;; Borrowed from doom-emacs: https://github.com/doomemacs/doomemacs
+(defun backbone-load-envvars-file (file &optional noerror)
+  "Read and set envvars from FILE.
+If NOERROR is non-nil, don't throw an error if the file doesn't exist or is
+unreadable. Returns the names of envvars that were changed."
+  (if (null (file-exists-p file))
+      (unless noerror
+        (signal 'file-error (list "No envvar file exists" file)))
+    (with-temp-buffer
+      (insert-file-contents file)
+      (when-let (env (read (current-buffer)))
+        (let ((tz (getenv-internal "TZ")))
+          (setq-default
+           process-environment
+           (append env (default-value 'process-environment))
+           exec-path
+           (append (split-string (getenv "PATH") path-separator t)
+                   (list exec-directory))
+           shell-file-name
+           (or (getenv "SHELL")
+               (default-value 'shell-file-name)))
+          (when-let (newtz (getenv-internal "TZ"))
+            (unless (equal tz newtz)
+              (set-time-zone-rule newtz))))
+        env))))
